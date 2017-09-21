1c54965d89f26e53ffe70691b1220fe20d5adf40
GuilleBazzi <guillermobazzi@gmail.com>
2017-09-21 11:32:33 -0300

    appv4

--- test.py
+++ test.py
@@ -49,17 +49,40 @@ def webhook():
     return r
 
 
+def processRequest(req):
+    if req.get("result").get("action") != "test.guille":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "item.cost":
+    if req.get("result").get("action") != "shipping.cost":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    product = parameters.get("product")
+    zone = parameters.get("shipping-zone")
 
-    # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-	
-	products = {'Aldactone':50.38, 'Aldazida':41.33, 'Aromasin':1025.58, 'Benefix':2022.29, 'Carduran':309.49, 'Cartrax Creme':70.06, 'Caverject ':102.88, 'Celebra':158.18, 'Champix':983.54, 'Citalor':199.82, 'Dalacin':126.60, 'Depo Provera':12.83, 'Depo-Medrol':18.62, 'Detrusitol':377.06, 'Diabinese':54.84, 'Dostinex':356.28, 'Ecalta':556.90, 'Eliquis':283.52, 'Farlutal':30.04, 'Feldene':28.66, 'Frademicina':20.54, 'Fragmin':211.91, 'Frontal':130.54, 'Genotropin':1702.91, 'Geodon':717.65, 'Inlyta':22327.77, 'Lipitor':343.95, 'Loniten':52.86, 'Lopid':85.34, 'Lyrica':212.22, 'Minidiab':39.19, 'Minipress':53.69, 'Motrin':18.62, 'Neurontin':225.68, 'NIMENRIX':260.31, 'Norvasc ':231.53, 'Olmetec':147.44, 'Pletil':23.42, 'Ponstan':29.64, 'Prolift':151.49, 'Propil':26.53, 'Provera':30.84, 'Revatio':3304.13, 'Sermion':124.61, 'Somavert':15646.40, 'Terra-Cortril':18.23, 'Terramicina':35.36, 'Tralen':40.64, 'Trofodermin':56.90, 'Unasyn':2641.26, 'Vfend':1743.55, 'Viagra':141.47, 'Vibramicina ':94.10, 'Vyndaqel':25783.42, 'Xalacom':185.16, 'Xalatan':164.81, 'Xalkori':28089.92, 'Xeljanz':4730.77, 'Zitromax':30.97, 'Zoloft':212.14, 'Zoltec':139.07, 'Zyvox':3767.04}
-    speech = "The cost of " + product + " is " + str(products[product]) + " euros."
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
